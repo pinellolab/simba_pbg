c7952f6123135804e1b25944766ccf5d90fdf0a2
huidongchen <hd7chen@gmail.com>
2021-09-30 13:43:24 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -7,12 +7,19 @@
 # LICENSE.txt file in the root directory of this source tree.
 
 import os
-
 from setuptools import setup
-from torch.utils import cpp_extension
+import subprocess
+import sys
+
+
+def install():
+    subprocess.check_call([sys.executable, "-m", "pip", "install",
+                           'torch', 'torchvision'])
 
 
 if __name__ == "__main__":
+    install()
+    from torch.utils import cpp_extension
     if int(os.getenv("PBG_INSTALL_CPP", 0)) == 0:
         setup()
     else:
@@ -22,5 +29,7 @@ if __name__ == "__main__":
                     "torchbiggraph._C", ["torchbiggraph/util.cpp"]
                 )
             ],
-            cmdclass={"build_ext": cpp_extension.BuildExtension},
+            cmdclass={
+                "build_ext": cpp_extension.BuildExtension
+                },
         )
